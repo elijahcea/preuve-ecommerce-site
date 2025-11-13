4a26e3e44d98728acb380b0c19f98ee8eb2edc52
elijahcea <elijah.cea01@gmail.com>
2025-11-13 12:00:48 -0500

    create ProductCard component

---
+++ src/components/product-card.tsx
@@ -0,0 +1,12 @@
+interface ProductCardProps {
+    id: string;
+    name: string;
+    price: number;
+    imageUrl: string;
+}
+
+export default function ProductCard({ id, name, price, imageUrl }: ProductCardProps) {
+    return (
+        <></>
+    )
+}
