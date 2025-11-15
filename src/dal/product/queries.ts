26de46a5315d993588339d0ef8410e3e444d6f66
elijahcea <elijah.cea01@gmail.com>
2025-11-14 19:51:13 -0500

    update getProducts to take cursor and collection

--- src/dal/product/queries.ts
+++ src/dal/product/queries.ts
@@ -1,18 +1,47 @@
 import prisma from "@/lib/prisma";
 
-export default async function GetProducts({ page, pageSize }: { page: number, pageSize: number }) {
+interface getProductParams {
+    cursor?: string, // id of last product from previous page
+    pageSize?: number,
+    collectionSlug?: string
+}
+
+export default async function getProducts({ 
+    cursor, 
+    pageSize = 20,
+    collectionSlug,
+ }: getProductParams) {
+
+    if (pageSize < 1 || pageSize > 100) {
+        throw new Error("pageSize must be between 1 and 100");
+    }
+
     try {
-        const skip = page * pageSize;
         const products = await prisma.product.findMany({
+            take: pageSize,
+            skip: cursor ? 1 : undefined,
+            cursor: {
+                id: cursor
+            },
+            where: collectionSlug ? {
+                collections: {
+                  some: {
+                    slug: collectionSlug, // Filter by collection
+                  },
+                },
+            } : undefined,
             orderBy: {
                 createdAt: "desc",
                 id: "desc"
-            },
-            take: pageSize,
-            skip: skip
+            }
         })
 
-        return products;
+        const nextCursor = products[products.length - 1].id
+
+        return {
+            products,
+            nextCursor
+        };
     } catch (e) {
         throw e;
     }
