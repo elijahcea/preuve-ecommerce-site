2c26d1f480053de8f277d068fad5dc5986b74d5a
elijahcea <elijah.cea01@gmail.com>
2025-11-18 13:14:19 -0500

    import Footer to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Header from "@/src/components/header";
+import Footer from "../components/footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className={`antialiased`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
