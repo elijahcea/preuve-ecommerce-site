615664464ded7e7c8f080d6315681f40077918a4
elijahcea <elijah.cea01@gmail.com>
2025-11-17 20:56:48 -0500

    add query to ProductPage

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,4 +1,12 @@
-export default function ProductSlug() {
+import { getProductBySlug } from "@/src/dal/product/queries";
+
+export default async function ProductPage({
+    params,
+  }: {
+    params: Promise<{ slug: string }>
+  }) {
+    const { slug } = await params;
+    const product = await getProductBySlug(slug);
     return (
         <p>This is a Product Slug page.</p>
     )
