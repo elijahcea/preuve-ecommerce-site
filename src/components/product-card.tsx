f41c9c0ba676763358f0029b51c0f4ebafd421ca
elijahcea <elijah.cea01@gmail.com>
2025-11-17 19:01:03 -0500

    add jsx for ProductCard

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -6,6 +6,10 @@ interface productCardProp {
 
 export default function ProductCard({ product }: productCardProp) {
     return (
-        <></>
+        <div>
+            <p>{product.name}</p>
+            <p>{product.basePrice}</p>
+            <p>{product.description}</p>
+        </div>
     )
 }
