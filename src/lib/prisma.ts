cd31e1e9fb9369619aa2652af2c9d4627a474930
elijahcea <elijah.cea01@gmail.com>
2025-11-19 12:29:24 -0500

    pass in prisma postgres adaptor to PrismaClient instance

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,17 @@
-import { PrismaClient } from '../../generated/prisma'
+import { PrismaClient } from "@/src/generated/prisma/client"
+
+// Driver Adapter for Postgres
+import { PrismaPg } from "@prisma/adapter-pg"
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!,
+});
 
 const globalForPrisma = global as unknown as { 
     prisma: PrismaClient
 }
 
-const prisma = globalForPrisma.prisma || new PrismaClient()
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
