7e68c1a49e64877be2e329ff8d067117a22b718e
elijahcea <elijah.cea01@gmail.com>
2025-11-24 15:51:56 -0500

    init variant-selector component

---
+++ src/components/variant-selector.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import clsx from 'clsx';
+import { useProductContext } from './product-provider';
+import { ProductOption, ProductVariantForDisplay } from '../lib/types';
+
+type Combination = {
+  id: string;
+  availableForSale: boolean;
+  [key: string]: string | boolean;
+};
+
+export default function VariantSelector({
+  options,
+  variants
+}: {
+  options: ProductOption[]
+  variants: ProductVariantForDisplay[]
+}) {
+  const { state, updateOption, updateURL } = useProductContext();
+  const hasNoOptionsOrJustOneOption =
+    !options.length || (options.length === 1 && options[0]?.values.length === 1);
+
+  if (hasNoOptionsOrJustOneOption) {
+    return null;
+  }
+
+  return (
+    <div>
+      {options.map(option => {
+        const optionNameLowerCase = option.name.toLowerCase();
+
+        return (
+          <fieldset key={option.name}>
+            <legend>{option.name}</legend>
+            {option.values.map(value => {
+              const isActive = state[optionNameLowerCase] === value;
+
+              return (
+                <button
+                  key={value}
+                  onClick={() => {
+                    const newState = updateOption(optionNameLowerCase, value);
+                    updateURL(newState);
+                  }}
+                >
+                  {isActive ? `Active! ${value}` : value }
+                </button>
+              )
+            })}
+          </fieldset>
+        )
+      })}
+    </div>
+  )
+}
