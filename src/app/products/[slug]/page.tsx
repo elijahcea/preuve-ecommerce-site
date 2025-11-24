137c7638bb61c227fa8e3ece32594c58bfcf6610
elijahcea <elijah.cea01@gmail.com>
2025-11-24 15:53:20 -0500

    update product detail page with new components

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,19 +1,24 @@
-import { getProductBySlug } from "@/src/dal/product/queries";
+import ProductDetail from "@/src/components/product-detail";
+import ProductProvider from "@/src/components/product-provider";
+import { ProductDetailSkeleton } from "@/src/components/skeletons";
+import { getProductForDisplay } from "@/src/dal/product/queries";
+import { ProductForDisplay } from "@/src/lib/types";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
 export default async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
-    const product = await getProductBySlug(slug);
+    const product: ProductForDisplay | null = await getProductForDisplay(slug);
 
     if (!product) {
       return notFound();
     }
 
     return (
-        <div>
-            <p>{product.name}</p>
-            <p>{product.description}</p>
-            <p>{product.basePrice}</p>
-        </div>
+        <ProductProvider>
+            <Suspense fallback={<ProductDetailSkeleton />}>
+                <ProductDetail product={product}/>
+            </Suspense>
+        </ProductProvider>
     )
 }
