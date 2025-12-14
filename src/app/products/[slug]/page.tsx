059b38ed3f42d7495fe1e968a461777d5310e0f5
elijahcea <elijah.cea01@gmail.com>
2025-12-14 12:08:00 -0500

    fix imports

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,14 +1,14 @@
-import ProductDetail from "@/src/components/product-detail";
-import ProductProvider from "@/src/components/product-provider";
+import ProductDetail from "@/src/components/product/product-detail";
+import ProductProvider from "@/src/contexts/product-provider";
 import { ProductDetailSkeleton } from "@/src/components/skeletons";
-import { getProductForDisplay } from "@/src/dal/product/queries";
-import { ProductForDisplay } from "@/src/lib/types";
+import { getProduct } from "@/src/dal/product/queries";
+import { Product } from "@/src/lib/types";
 import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
 export default async function ProductPage({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
-    const product: ProductForDisplay | null = await getProductForDisplay(slug);
+    const product: Product | null = await getProduct(slug);
 
     if (!product) {
       return notFound();
