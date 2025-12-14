88e16dcf75af2835f7cdbb71055350db8b46a09c
elijahcea <elijah.cea01@gmail.com>
2025-12-14 12:22:20 -0500

    add Cart types and update and rename product types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,40 +1,61 @@
-import { Prisma } from "../generated/prisma/client";
-
 export type ProductOption = {
   name: string;
-  slug: string;
   values: string[];
 };
 
-export type VariantSelectedOption = {
+export type SelectedOption = {
   name: string;
   value: string;
 };
 
-export type ProductVariantForDisplay = {
+export type ProductVariant = {
   id: string;
   sku: string;
   price: number;
-  stock: number;
-  isActive: boolean;
+  isAvailableForSale: boolean;
   imageUrl: string | null;
-  selectedOptions: VariantSelectedOption[];
+  selectedOptions: SelectedOption[];
 };
 
-export type ProductForDisplay = {
+export type Product = {
   id: string;
   slug: string;
   name: string;
   description: string;
-  basePrice: number;
-  baseStock: number;
   imageUrl: string;
-  isActive: boolean;
+  isAvailableForSale: boolean;
   createdAt: Date;
   updatedAt: Date;
   
   // Transformed/computed fields
   options: string[];  // ["Color", "Size"]
-  optionsWithValues: ProductOption[];  // [{ name: "Color", slug: "color", values: [...] }]
-  variants: ProductVariantForDisplay[];
+  optionsWithValues: ProductOption[];  // [{ name: "Color", values: [...] }]
+  variants: ProductVariant[];
 };
+
+export type Cart = {
+  id: string | undefined;
+  items: CartItem[];
+  totalQuantity: number;
+  cost: {
+    totalCost: number;
+    subtotalCost: number;
+    totalTaxAmount: number;
+  }
+};
+
+export type CartItem = {
+  id: string | undefined;
+  totalCost: number;
+  quantity: number;
+  merchandise: Merchandise;
+};
+
+export type Merchandise = {
+  id: string;
+  name: string;
+  price: number;
+  isAvailableForSale: boolean;
+  imageUrl: string | null;
+  selectedOptions: SelectedOption[];  
+}
