7729ffd6f19fac5336ebe0e7421b00962f18d975
elijahcea <elijah.cea01@gmail.com>
2025-12-14 12:07:32 -0500

    update root layout.tsx with CartProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,29 @@ import type { Metadata } from "next";
 import Header from "@/src/components/header";
 import Footer from "../components/footer";
 import "./globals.css";
+import { CartProvider } from "../contexts/cart-provider";
+import { getCart } from "../dal/cart/queries";
 
 export const metadata: Metadata = {
   title: "Preuve New York",
   description: "Be The Proof You Want To See",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cartPromise = getCart();
+
   return (
     <html lang="en">
       <body className={`antialiased`}>
-        <Header />
-        {children}
-        <Footer />
+        <CartProvider cartPromise={cartPromise}>
+          <Header />
+          {children}
+          <Footer />
+        </CartProvider>
       </body>
     </html>
   );
