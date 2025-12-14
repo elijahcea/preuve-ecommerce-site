478f4b99de4725b369156f074d0d711c5923aa57
elijahcea <elijah.cea01@gmail.com>
2025-12-14 12:20:59 -0500

    add cart helper functions

---
+++ src/dal/cart/helpers.ts
@@ -0,0 +1,18 @@
+import { CartItem } from "@/src/lib/types";
+
+export function calculateCartTotals(items: CartItem[]) {
+    return items.reduce((totals, curr) => {
+        return {
+            totalQuantity: totals.totalQuantity + curr.quantity,
+            cost: {
+                totalCost: totals.cost.totalCost + curr.totalCost,
+                subtotalCost: totals.cost.subtotalCost + curr.totalCost,
+                totalTaxAmount: 0
+            }
+        }
+    }, { totalQuantity: 0, cost: { totalCost: 0, subtotalCost: 0, totalTaxAmount: 0 }})
+}
+
+export function calculateItemCost(price: number, quantity: number) {
+    return price * quantity;
+}
