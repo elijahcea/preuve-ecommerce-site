3b2af334f5abcc1f4ef3e2aecfd7f2ee3a7fd192
elijahcea <elijah.cea01@gmail.com>
2025-12-14 12:07:43 -0500

    fix import path

--- src/app/collections/shop-all/page.tsx
+++ src/app/collections/shop-all/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react"
-import ProductList from "@/src/components/product-list"
+import ProductList from "@/src/components/product/product-list"
 import { ProductListSkeleton } from "@/src/components/skeletons"
 
 export default async function ShopAll() {
