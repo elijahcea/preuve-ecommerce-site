413b37ede1d5235c05636345f669def3d31d2409
elijahcea <elijah.cea01@gmail.com>
2025-12-17 09:40:59 -0500

    create EditItemQuantityBtn

---
+++ src/components/cart/edit-item-quantity-btn.tsx
@@ -0,0 +1,35 @@
+import { updateItemAction } from "@/src/actions/cart";
+import { ItemAction, useCartContext } from "@/src/contexts/cart-provider"
+import { CartItem } from "@/src/lib/types";
+import { useActionState } from "react";
+import { MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
+
+export default function EditItemQuantityBtn({ item, action }: { item: CartItem, action: ItemAction }) {
+    const { updateCartItem } = useCartContext();
+    const [message, formAction, isPending] = useActionState(updateItemAction, undefined);
+    const updateItem = formAction.bind(null, { item, action });
+
+    return (
+        <form action={async () => {
+            await updateItem();
+            updateCartItem(item.merchandise.id, action);
+        }}>
+            <button 
+                aria-disabled={isPending}
+                disabled={isPending}
+                className="cursor-pointer font-medium text-indigo-600 hover:text-indigo-500"
+            >
+                {action === "delete" ? <p>Remove</p>
+                    : (action === "plus" ?
+                        <PlusIcon className="size-4 text-gray-900" />
+                        :
+                        <MinusIcon className="size-4 text-gray-900" />
+                    )
+                }
+            </button>
+            {message && (
+                <p>{message}</p>
+            )}
+        </form>
+    )
+}
