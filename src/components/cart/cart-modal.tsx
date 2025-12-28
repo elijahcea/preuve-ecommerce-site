4a0a7e5637ed85d8e7dfbe5469027895334be706
elijahcea <elijah.cea01@gmail.com>
2025-12-28 14:47:56 -0500

    fix spaces

--- src/components/cart/cart-modal.tsx
+++ src/components/cart/cart-modal.tsx
@@ -19,9 +19,9 @@ export default function CartModal() {
     const currency = "USD";
 
     useEffect(() => {
-        if (!cart) {
-            createCartAndSetCookie();
-        }
+      if (!cart) {
+        createCartAndSetCookie();
+      }
     }, [cart]);
 
   return (
