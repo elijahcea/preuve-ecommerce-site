845303236bf2fc51039046638e19fc2962187d5a
elijahcea <elijah.cea01@gmail.com>
2025-12-28 14:14:59 -0500

    update addToCart styling

--- src/components/cart/add-to-cart-btn.tsx
+++ src/components/cart/add-to-cart-btn.tsx
@@ -5,6 +5,7 @@ import { useCartContext } from "@/src/contexts/cart-provider";
 import { Product, ProductVariant } from "@/src/lib/types";
 import { useActionState } from "react";
 import LoadingSpinner from "../loading-spinner";
+import { ShoppingBagIcon } from "@heroicons/react/24/outline";
 
 export default function AddToCartBtn({ variant, product }: { variant: ProductVariant | undefined, product: Product }) {
     const { addCartItem } = useCartContext();
@@ -21,20 +22,23 @@ export default function AddToCartBtn({ variant, product }: { variant: ProductVar
                 aria-disabled={!variant?.isAvailableForSale || isPending}
                 disabled={!variant?.isAvailableForSale || isPending}
                 className={`
-                    w-full px-6 py-3 rounded-lg font-semibold transition-colors
-                    ${variant?.isAvailableForSale
-                        ? "bg-blue-600 text-white hover:bg-blue-700 cursor-pointer"
-                        : "bg-gray-300 text-gray-500 cursor-not-allowed"
+                    w-full px-6 py-3 rounded-lg font-semibold transition-opacity ease-in-out duration-250 text-white bg-foreground
+                    ${!variant?.isAvailableForSale || isPending
+                        ? "cursor-not-allowed opacity-50"
+                        : "cursor-pointer hover:opacity-75"
                     }
                 `}
                 type="submit"
             >
-                {!variant 
-                    ? "Please select all options"
-                    : variant.isAvailableForSale
-                        ? (isPending ? <LoadingSpinner /> : "Add to cart")
-                        : "Out of stock"
-                }
+                <span className="flex gap-2 items-center justify-center">
+                    <ShoppingBagIcon aria-hidden="true" className="size-5" />
+                    {!variant 
+                        ? "SELECT OPTIONS"
+                        : variant.isAvailableForSale
+                            ? (isPending ? <LoadingSpinner /> : "ADD TO CART")
+                            : "OUT OF STOCK"
+                    }
+                </span>
             </button>
             {message && (
                 <p>{message}</p>
