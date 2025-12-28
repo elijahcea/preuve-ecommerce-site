8210926b83b64f99146877f57fad5997b3fa5205
elijahcea <elijah.cea01@gmail.com>
2025-12-28 14:16:09 -0500

    add image to productCard

--- src/components/product/product-card.tsx
+++ src/components/product/product-card.tsx
@@ -1,11 +1,30 @@
 import Link from "next/link"
-import { Product } from "@/src/generated/prisma/client"
+import Image from "next/image"
+import Price from "../price"
+import { Product } from "@/src/lib/types"
 
-export default function ProductCard({ product }: { product: Product }) {
+export default function ProductCard({ product }: { product: Product }): React.ReactNode {
     return (
-        <div>
-            <Link href={`/products/${product.slug}`}>{product.name}</Link>
-            <p>{product.description}</p>
-        </div>
+        <li>
+            <Link href={`/products/${product.slug}`}>
+                <div className="flex flex-col gap-4">
+                    {product.featuredImage?.url ?
+                        <Image 
+                            src={product.featuredImage.url} 
+                            alt={product.featuredImage.altText || product.name}
+                            height={750}
+                            width={600}
+                            style={{ width: "100%", height: "auto"}}
+                        />
+                        : 
+                        <></>
+                    }
+                    <div className="flex justify-between text-sm">
+                        <p>{product.name}</p>
+                        <Price amount={product.priceRange.minVariantPrice} />
+                    </div>
+                </div>
+            </Link>
+        </li>
     )
 }
