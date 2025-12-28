0355c56ba8931071607c665b7a56adf05fd4f660
elijahcea <elijah.cea01@gmail.com>
2025-12-28 14:10:12 -0500

    prisma migrations

---
+++ prisma/migrations/20251217194253_replace_image_url_field_in_product_to_feature_image_url_and_alt/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `imageUrl` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "imageUrl",
+ADD COLUMN     "featuredImageAlt" TEXT,
+ADD COLUMN     "featuredImageURL" TEXT;
