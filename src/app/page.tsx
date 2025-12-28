671ce64718e8ba40d9e3122143ed36d108776303
elijahcea <elijah.cea01@gmail.com>
2025-12-28 14:12:23 -0500

    temp add images to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const images = [
+    "https://cdn.shopify.com/s/files/1/1600/6017/files/Screen_Shot_2024-05-31_at_6.25.32_PM.png?v=1717194503",
+    "https://cdn.shopify.com/s/files/1/1600/6017/files/Screen_Shot_2024-05-31_at_6.25.35_PM.png?v=1717194503",
+    "https://cdn.shopify.com/s/files/1/1600/6017/files/Screen_Shot_2024-05-31_at_6.25.45_PM.png?v=1717194503"
+  ]
   return (
-    <></>
+    <div className="m-auto columns-3 p-5">
+      {images.map(image => {
+        return ( 
+          <Link key={image} href="/collections/shop-all">
+            <div className="relative h-auto">
+              <Image
+                src={image}
+                alt="Homepage photo"
+                height={649}
+                width={432}
+              />
+            </div>
+          </Link>
+        )
+      })}
+    </div>
   );
 }
