17064cd125e044708923a2804be845c8d4cea7ee
elijahcea <elijah.cea01@gmail.com>
2026-01-26 19:22:35 -0500

    create collection dal query functions

--- src/dal/collection/queries.ts
+++ src/dal/collection/queries.ts
@@ -1,8 +1,31 @@
 import prisma from "@/src/lib/prisma";
+import { Collection, CollectionPreview } from "@/src/lib/types";
 
-export async function getAllCollections() {
-  const collections = await prisma.collection.findMany();
-  if (!collections) return undefined;
+export async function getAllCollections(): Promise<CollectionPreview[] | null> {
+  try {
+    const collections = await prisma.collection.findMany();
+    if (!collections) return null;
 
-  return collections;
+    return collections;
+  } catch (e) {
+    throw e;
+  }
+}
+
+export async function getCollection(id: string): Promise<Collection | null> {
+  try {
+    const collection = await prisma.collection.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        products: true,
+      },
+    });
+    if (!collection) return null;
+
+    return collection;
+  } catch (e) {
+    throw e;
+  }
 }
