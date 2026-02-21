edd7905bce118901aace118d56d4d6502688b611
elijahcea <elijah.cea01@gmail.com>
2026-02-21 13:55:34 -0500

    add mutations for creating a product and variants

--- src/dal/product/mutations.ts
+++ src/dal/product/mutations.ts
@@ -1,3 +1,30 @@
 import "server-only";
 
 import prisma from "@/src/lib/prisma";
+import { Product, ProductCreateInput } from "@/src/lib/types";
+import { createProductInput, includeProductAllRelations } from "./prismaTypes";
+import { formatProduct, checkHasOnlyDefaultVariant } from "./utils";
+
+export async function createProductWithOptions(
+  input: ProductCreateInput,
+): Promise<Product> {
+  const { product } = input;
+
+  const hasOnlyDefaultVariant = checkHasOnlyDefaultVariant(input);
+
+  const newProduct = await prisma.product.create({
+    data: createProductInput(
+      product.status,
+      product.title,
+      product.description,
+      hasOnlyDefaultVariant,
+      product.collectionIds,
+      product.options,
+    ),
+    include: includeProductAllRelations,
+  });
+
+  const formattedProduct = formatProduct(newProduct);
+
+  return formattedProduct;
+}
---
+++ src/dal/productVariant/mutations.ts
@@ -0,0 +1,33 @@
+import prisma from "@/src/lib/prisma";
+import {
+  createProductVariantInput,
+  includeProductVariantWithOptionValues,
+} from "./prismaTypes";
+import { ProductOption, VariantOptionValueInput } from "@/src/lib/types";
+import { formatVariant } from "./utils";
+import { getProduct } from "../product/queries";
+
+export async function createProductVariant(
+  productId: string,
+  productOptions: ProductOption[],
+  sku: string,
+  price: number,
+  inventoryQuantity: number,
+  variantOptionValues: VariantOptionValueInput[],
+) {
+  const product = await getProduct({ id: productId });
+  if (!product) throw new Error(`Product with ID: ${productId} does not exist`);
+
+  const newVariant = await prisma.productVariant.create({
+    data: createProductVariantInput(
+      productId,
+      productOptions,
+      sku,
+      price,
+      inventoryQuantity,
+      variantOptionValues,
+    ),
+    include: includeProductVariantWithOptionValues,
+  });
+  return formatVariant(product.title, product.slug, newVariant);
+}
