eb8ccd285bded2c3b7179de9e3cdc59a55b55064
elijahcea <elijah.cea01@gmail.com>
2026-03-07 16:18:07 -0500

    drop slug field from option value model

---
+++ prisma/migrations/20260304182529_drop_slug_field_in_product_option_value_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `slug` on the `ProductOptionValue` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "ProductOptionValue" DROP COLUMN "slug";
