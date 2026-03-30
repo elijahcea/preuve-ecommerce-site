bf11cb2e9bfc1ee505ead404303fb874371e4976
elijahcea <elijah.cea01@gmail.com>
2026-03-30 15:25:58 -0400

    add post and put collection api routes

--- src/app/api/collections/route.ts
+++ src/app/api/collections/route.ts
@@ -1,5 +1,7 @@
+import { createCollection } from "@/src/dal/collection/mutations";
 import { getAllCollections } from "@/src/dal/collection/queries";
-import { GetCollectionsResponse } from "@/src/lib/types";
+import { hasPermissions, validateToken } from "@/src/dal/utils";
+import { CollectionCreateDTO, GetCollectionsResponse } from "@/src/lib/types";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
@@ -17,6 +19,75 @@ export async function GET(
       headers: { "Content-Type": "application/json" },
     });
   } catch (e) {
+    return new NextResponse(JSON.stringify(`Internal server error: ${e}`), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const authHeader = request.headers.get("Authorization");
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "No token provided",
+        }),
+        {
+          status: 401,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
+
+    const token = authHeader.split(" ")[1];
+    const authPayload = await validateToken(token);
+
+    if (!authPayload) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "Invalid token",
+        }),
+        {
+          status: 401,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
+
+    const isAuthorized = hasPermissions(
+      authPayload.permissions as Array<string>,
+      "create:collections",
+    );
+
+    if (!isAuthorized) {
+      return new NextResponse(
+        JSON.stringify({
+          message: "Unauthorized",
+        }),
+        {
+          status: 401,
+          headers: { "Content-Type": "application/json" },
+        },
+      );
+    }
+
+    const body: CollectionCreateDTO = await request.json();
+
+    const newCollection = await createCollection(body);
+
+    const response = {
+      collection: newCollection,
+    };
+
+    return new NextResponse(JSON.stringify(response), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (e) {
+    console.log(e);
     return new NextResponse(JSON.stringify(`Internal server error: ${e}`), {
       status: 500,
       headers: { "Content-Type": "application/json" },
