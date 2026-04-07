828d7e54ab28f6225bcafbd89345f1c0118f741e
elijahcea <elijah.cea01@gmail.com>
2026-04-07 18:37:01 -0400

    init empty app routes with content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,25 @@
 export default function Contact() {
-  return <div>Contact us Page.</div>;
+  return (
+    <main className="flex flex-col gap-2 m-auto px-4 max-w-prose">
+      <h1 className="font-semibold text-lg">Contact Us</h1>
+      <p className="leading-7">
+        Have a question or need assistance? We’ve got you covered: Please reach
+        out at <span className="font-semibold">elijah.cea01@gmail.com.</span> We
+        aim to respond as quickly as possible, so don’t hesitate to get in
+        touch.
+      </p>
+      <p>
+        Like what you see? Vist{" "}
+        <a
+          className="font-semibold"
+          href="https://www.elijahcea.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          elijahcea.com
+        </a>{" "}
+        for more.
+      </p>
+    </main>
+  );
 }
